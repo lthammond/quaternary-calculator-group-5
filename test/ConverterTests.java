b6420ae2a886eed02fb120656ddea02bc5e0f391
Landon Hammond <lthammond@bsu.edu>
2022-09-07 19:57:53 -0400

    Add test folder to house logic tests

---
+++ test/ConverterTests.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ConverterTests {
+    @Test
+    public void testConvertToQuaternary1() {
+        int decimalNumber = 1;
+        Converter converter = new Converter();
+        String quaternaryNumber = converter.convertToQuaternary(decimalNumber);
+        Assertions.assertEquals("1", quaternaryNumber);
+    }
+
+    @Test
+    public void testConvertToQuaternary2() {
+        int decimalNumber = 4;
+        Converter converter = new Converter();
+        String quaternaryNumber = converter.convertToQuaternary(decimalNumber);
+        Assertions.assertEquals("10", quaternaryNumber);
+    }
+}
