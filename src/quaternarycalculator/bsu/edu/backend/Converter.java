74f6882b0732fbab5180a2c524e614a5cf61991f
Landon Hammond <lthammond@bsu.edu>
2022-09-07 23:42:55 -0400

    Refactor Converter class to better handle negative numbers

--- src/quaternarycalculator/bsu/edu/backend/Converter.java
+++ src/quaternarycalculator/bsu/edu/backend/Converter.java
@@ -6,27 +6,43 @@ public class Converter {
     public int convertToDecimal(String quaternaryNumberToConvert) {     // Converts base 4 number to base 10 number.
         int convertedDecimalNumber = 0;
         int placeValue = 1;
+        boolean isNegative = false;
+
+        if (quaternaryNumberToConvert.contains("-")) { // checks to see if base 4 number is negative and if so removes sign.
+            quaternaryNumberToConvert = quaternaryNumberToConvert.replace("-", "");
+            isNegative = true;
+        }
         for(int i = 0; i < quaternaryNumberToConvert.length(); i++) {
             int value = parseInt(String.valueOf(quaternaryNumberToConvert.charAt(quaternaryNumberToConvert.length()-i-1)));
-            // Reads value of string index from right to left
+            // Reads value of string index from right to left.
             convertedDecimalNumber += value * placeValue; // takes value in string index and multiplies it by place value.
-            placeValue = placeValue * 4;
+            placeValue *= 4;
         }
+        if (isNegative == true) // Returns negative sign to int after conversion.
+            convertedDecimalNumber *= -1;
         return convertedDecimalNumber;
     }
 
     public String convertToQuaternary(int decimalNumberToConvert) {     // Converts base 10 number to base 4 number.
-        StringBuilder stringBuilder = new StringBuilder(); // StringBuilder is used to append remainder.
+        StringBuilder remainderList = new StringBuilder(); // StringBuilder is used to append remainders to list.
+        boolean isNegative = false;
         if(decimalNumberToConvert == 0) {
             return "0";
         } else {
+            if(decimalNumberToConvert < 0) {
+                decimalNumberToConvert *= -1;
+                isNegative = true;
+            }
             while (decimalNumberToConvert != 0) {
                 int quotient = decimalNumberToConvert / 4;
                 int remainder = decimalNumberToConvert % 4;
                 decimalNumberToConvert = quotient;
-                stringBuilder.append(remainder);
+                remainderList.append(remainder);
+                }
+            if (isNegative) {
+                remainderList.append("-");
             } // Remainder string has been appended backwards and needs to be reversed.
-            return String.valueOf(stringBuilder.reverse());
+            return String.valueOf(remainderList.reverse());
         }
     }
 }
