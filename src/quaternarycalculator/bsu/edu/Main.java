e320ee87712743bb746e8541b5ec33136a36fee5
michaelknudsen <mknudsen762@gmail.com>
2022-09-08 10:55:42 -0400

    Changed QuaternaryCalculator class to inherit from JFrame

--- src/quaternarycalculator/bsu/edu/Main.java
+++ src/quaternarycalculator/bsu/edu/Main.java
@@ -4,7 +4,8 @@ import quaternarycalculator.bsu.edu.frontend.QuaternaryCalculator;
 
 public class Main {
     public static void main(String[] args)  {
-        QuaternaryCalculator calc = new QuaternaryCalculator();
-        calc.start();
+        QuaternaryCalculator frame = new QuaternaryCalculator();
+        frame.setVisible(true);
+
     }
 }
--- src/quaternarycalculator/bsu/edu/frontend/QuaternaryCalculator.java
+++ src/quaternarycalculator/bsu/edu/frontend/QuaternaryCalculator.java
@@ -2,17 +2,11 @@ package quaternarycalculator.bsu.edu.frontend;
 
 import javax.swing.*;
 
-public class QuaternaryCalculator {
+public class QuaternaryCalculator extends JFrame{
 
     public QuaternaryCalculator(){
+        this.setSize(600, 800);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    }
-    public void start(){
-        JFrame frame = new JFrame("My First GUI");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
-        JButton button = new JButton("Press");
-        frame.getContentPane().add(button); // Adds Button to content pane of frame
-        frame.setVisible(true);
     }
 }
